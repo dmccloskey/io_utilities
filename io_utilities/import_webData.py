04b4743be6e6721dfc4ca7ee29592d6a781a849a
Douglas McCloskey <dmccloskey87@gmail.com>
2016-10-16 16:42:33 -0700

    updates to get_request

--- io_utilities/import_webData.py
+++ io_utilities/import_webData.py
@@ -1,4 +1,4 @@
-from .base_importData import base_importData
+﻿from .base_importData import base_importData
 #System dependences (get_request)
 import requests, sys
 #System dependencies (ftp)
@@ -6,10 +6,35 @@ from ftplib import FTP
 import io
 
 class import_webData(base_importData):
+    def decode_request(
+        self,
+        request_I):
+        ''' 
+        decode request
+        INPUT:
+        request_I
+        OUTPUT:
+        decoded = decoded request object
+        EXAMPLE:
+        decoded = decode_request(request_I)
+        '''
+
+        decoded = None;
+        #parse based on the headers
+        if 'text/plain' in request_I.headers["Content-Type"]:
+            decoded = request_I.text;
+        elif "application/json" in request_I.headers["Content-Type"]:
+            decoded = request_I.json()
+        elif 'binary' in request_I.headers["Content-Type"]:
+            decoded = request_I.content
+        return decoded;
     def get_request(
         self,
         server,
-        ext):
+        ext,
+        request_parameters_I={},
+        raise_I=False,
+        verbose_I=True):
         ''' 
         get request using REST
         INPUT:
@@ -20,16 +45,58 @@ class import_webData(base_importData):
         EXAMPLE:
         server = "http://bigg.ucsd.edu"
         ext = "/api/v2/models/iJO1366/reactions"
-        decoded = get_request(server,ext)
+        request = get_request(server,ext)
         '''
-        decoded = None;
-        r = requests.get(server+ext, headers={ "Content-Type" : "application/json"})
+        r = requests.get(server+ext, **request_parameters_I)
+        #check the status code
+        if verbose_I: print("request status code " + str(r.status_code))
         if not r.ok:
-    #         r.raise_for_status()
-            pass
-        else:
-            decoded = r.json()
-        return decoded
+            if raise_I: r.raise_for_status();
+            return None;
+        #check the encoding
+        if verbose_I: print(r.encoding);
+        return r;
+    def parse_textRequest(
+        self,
+        request,
+        iter_lines_parameters_I = {},
+        encoding = 'utf-8',
+        deliminator = '\t',
+        newline = '\n',
+        headers = [],
+        n_lines_metadata = 0):
+        '''
+        Parse a binary file
+        INPUT:
+        file = binary file
+        encoding = string, encoding type
+        deliminator = string, column deliminator
+        newline = string, new line deliminator
+        headers = list, list of headers
+            if headers = [], first row will be treated as the keys build the dictionary
+        n_lines_metadata = integer, number of metadata lines to skip
+        OUTPUT:
+        rows_O = [{}] of parsed and decoded elements
+        '''
+        rows_O = [];
+        cnt = 0;
+        for line in request.iter_lines(decode_unicode=True,delimiter=newline,**iter_lines_parameters_I):
+            #check for metadata
+            if cnt<n_lines_metadata:
+                cnt+=1;
+                continue;
+            #parse data                
+            #print(text)
+            row = line.split(deliminator)
+            #print(row)
+            #check for headers
+            if not headers:
+                headers = row;
+            else:
+                rows_O.append(dict(zip(headers, row)))
+        request.close();
+        return rows_O;
+
     def access_ftp(self,server,username=None,password=None):
         ''' 
         access ftp server
@@ -77,7 +144,6 @@ class import_webData(base_importData):
         sio.seek(0)
         ftp.quit()
         return sio;
-
     def post_ftp(self,server,ext,filename,username=None,password=None):
         '''
         post ftp file to ftp server
@@ -102,7 +168,6 @@ class import_webData(base_importData):
         #upload the file
         ftp.storbinary('STOR '+filename, open(filename, 'rb'))
         ftp.quit()
-
     def parse_binaryFile(
         self,
         file,
